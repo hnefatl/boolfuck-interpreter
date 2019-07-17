9dc9882df4fc22172838ac6a38dee1d8b4d79ec9
Keith Collister <hnefatl@gmail.com>
2019-07-17 11:38:42 +0100

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@
 // https://www.codewars.com/kata/5861487fdb20cff3ab000030
 
 struct State {
-    pos: i32, // Current position of the head on the tape
+    pos: i32,            // Current position of the head on the tape
     pos_tape: Vec<bool>, // Positions 0, 1, ...
     neg_tape: Vec<bool>, // Positions -1, -2, ...
-    input: Vec<u8>, // Input stream
-    input_bit: usize, // Next bit in the input stream to read
-    output: Vec<u8>, // Output stream
-    output_bit: usize, // Next index in the output stream to write to
-    code: Vec<char>, // The code string
-    code_index: usize, // The program counter/index into the code string
+    input: Vec<u8>,      // Input stream
+    input_bit: usize,    // Next bit in the input stream to read
+    output: Vec<u8>,     // Output stream
+    output_bit: usize,   // Next index in the output stream to write to
+    code: Vec<char>,     // The code string
+    code_index: usize,   // The program counter/index into the code string
 }
 fn initialise_state(code: Vec<char>, input: Vec<u8>) -> State {
     State {
@@ -30,8 +30,7 @@ impl State {
         let r;
         if self.pos >= 0 {
             r = State::get_or_extend_mut(&mut self.pos_tape, self.pos as usize)
-        }
-        else {
+        } else {
             r = State::get_or_extend_mut(&mut self.neg_tape, -self.pos as usize - 1)
         }
         *r = bit;
@@ -44,10 +43,15 @@ impl State {
     }
     fn get_bit(&self) -> bool {
         if self.pos >= 0 {
-            self.pos_tape.get(self.pos as usize).map(|b| *b).unwrap_or(false)
-        }
-        else {
-            self.neg_tape.get(-self.pos as usize - 1).map(|b| *b).unwrap_or(false)
+            self.pos_tape
+                .get(self.pos as usize)
+                .copied()
+                .unwrap_or(false)
+        } else {
+            self.neg_tape
+                .get(-self.pos as usize - 1)
+                .copied()
+                .unwrap_or(false)
         }
     }
 
@@ -77,26 +81,23 @@ impl State {
             match_char = ']';
             direction = 1;
             position_adjust = 1; // Jump to one past the [, in accordance with the spec
-        }
-        else if init_char == ']' {
+        } else if init_char == ']' {
             // Look for [ to the left
             match_char = '[';
             direction = -1;
             position_adjust = 0; // Jump to exactly on the ]
-        }
-        else {
+        } else {
             panic!("Character passed is neither '[' nor ']'")
         }
 
         let mut code_index = self.code_index;
         // Count the number of brackets of the same type as init_char that we need to see before being able to accept
-        // a match_char as being the *closing* bracket 
+        // a match_char as being the *closing* bracket
         let mut mismatch_count: u32 = 0;
         loop {
             if code_index == 0 {
                 panic!("Reached start of code while looking for {}", match_char)
-            }
-            else if code_index + 1 == self.code.len() {
+            } else if code_index + 1 == self.code.len() {
                 panic!("Reached end of code while looking for {}", match_char)
             }
             // Checks above ensure we don't over/underflow
@@ -105,13 +106,11 @@ impl State {
             if self.code[code_index] == init_char {
                 // Mismatching bracket, we need to see one more opposite bracket
                 mismatch_count += 1;
-            }
-            else if self.code[code_index] == match_char {
+            } else if self.code[code_index] == match_char {
                 // Matching bracket, but is it the one for us?
                 if mismatch_count > 0 {
                     mismatch_count -= 1;
-                }
-                else {
+                } else {
                     break; // Found the matching bracket
                 }
             }
@@ -125,16 +124,17 @@ impl State {
             Some(&command) => {
                 match command {
                     '+' => self.set_bit(!self.get_bit()), // Flip the bit under the cursor
-                    ',' => { // Set the bit under the cursor from the input stream
+                    ',' => {
+                        // Set the bit under the cursor from the input stream
                         let b = self.get_input_bit();
                         self.set_bit(b);
-                    },
+                    }
                     ';' => self.push_output_bit(self.get_bit()), // Output the bit under the cursor
                     '<' => self.pos -= 1, // Move the pointer one bit to the left
                     '>' => self.pos += 1, // Move the pointer one bit to the right
                     '[' if !self.get_bit() => self.jump_to_matching_bracket('['),
-                    ']' if  self.get_bit() => self.jump_to_matching_bracket(']'),
-                    _ => ()
+                    ']' if self.get_bit() => self.jump_to_matching_bracket(']'),
+                    _ => (),
                 }
                 // If we did a jump operation, don't move to the next instruction, we're already at it
                 if command != '[' && command != ']' {
@@ -150,6 +150,5 @@ fn main() {
     let code = "";
     let input = vec![];
     let mut state = initialise_state(code.chars().collect(), input);
-    while state.step() {
-    }
+    while state.step() {}
 }
